35697075f6184274280fa884a54541bca4184137
YuriBae0106 <106811326+YuriBae0106@users.noreply.github.com>
2026-06-22 17:19:28 +0900

    add tagsystem example

--- example/tagsystem.jl
+++ example/tagsystem.jl
@@ -14,10 +14,11 @@ function makesnapshot(; optimize::Bool=false)
     )
     snap = Snapshot(sim, opts)
     elec = Node("ZONE1", EnergyCarrier("electricity ZONE1", sim), rule=:curtailed, evalprice=true, losses=0.0, tags=[:electricity])
+    elec2 = Node("ZONE2",EnergyCarrier("electricity ZONE2", sim), rule=:curtailed, evalprice=true, losses=0.0, tags=[:electricity])
     co2 = Node("CO2", CO2Carrier("CO2", sim), rule=:curtailed, tags=[:co2])
 
-    makedispatchable("CCGT", "CCGT", elec, co2, snap; cap=100.0, construction_profile=1.0)
-    makeintermittentsource("Onwind gen", "Onwind", elec, co2, snap; cap=100.0, weatheryear=2019, construction_profile=1.0)
+    makedispatchable("CCGT", "CCGT", elec, co2, snap; cap=100.0, construction_profile=1.0, decommissioning_profile=1.0)
+    makeintermittentsource("Onwind gen", "Onwind", elec, co2, snap; cap=100.0, weatheryear=2019, construction_profile=1.0, decommissioning_profile=1.0)
     makebatteries(
         "Battery", "Battery", elec, snap;
         capin=100.0, eff=0.9, duration=4.0,
@@ -25,12 +26,12 @@ function makesnapshot(; optimize::Bool=false)
         construction_profile=1.0, connection_cost=0.0, om_var_cost=1.0,
     )
 
-    !optimize && return snap, elec, co2
+    !optimize && return snap, elec, elec2, co2
     Nosy.optimize!(snap, cost(snap))
-    return extract(snap), elec, co2
+    return extract(snap), elec, elec2, co2
 end
 
-s, elec, co2 = makesnapshot()
+s, elec, elec2, co2 = makesnapshot()
 wind = Nosy.getcomponent(s, "Onwind gen ZONE1")
 bat = Nosy.getcomponent(s, "Battery ZONE1")
 ccgt = Nosy.getcomponent(s, "CCGT ZONE1")
@@ -62,7 +63,7 @@ zone1_generation = Nosy.getcomponents(s, "ZONE1"; with=[:generation])
 @assert length(zone1_generation) == 3
 
 # Does a shared kind tag group two components even when names differ ("Onwind gen" vs "Plant A")
-makeintermittentsource("Plant A", "Onwind", elec, co2, s; cap=50.0, weatheryear=2019, construction_profile=1.0)
+makeintermittentsource("Plant A", "Onwind", elec, co2, s; cap=50.0, weatheryear=2019, construction_profile=1.0, decommissioning_profile=1.0)
 intermittent_all = Nosy.getcomponents(s; with=[:intermittent])
 @assert length(intermittent_all) == 2
 @assert haskey(intermittent_all, "Plant A ZONE1")
@@ -83,3 +84,9 @@ zone2_price_ics = Nosy.getcomponents(s; with=[:priceinterconnection, :ZONE2])
 zone1_price_ics = Nosy.getcomponents(s, "ZONE1"; with=[:priceinterconnection, :ZONE2])
 @assert length(zone1_price_ics) == 1
 @assert haskey(zone1_price_ics, Nosy.name(price_ic))
+
+# Does makenodeinterco attach :interconnection and :nodeinterconnection?
+node_ic = makenodeinterco("IC name", elec, elec2, 100.0, 100.0, s)
+
+@assert Nosy.hastag(node_ic, :interconnection)
+@assert Nosy.hastag(node_ic, :nodeinterconnection)
